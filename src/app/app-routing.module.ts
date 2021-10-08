4ffbbc294479c9736e0018d7e98756af5ddab028
ad.pichugin <ad.pichugin@sdek.ru>
2021-10-08 12:42:14 +0700

    Moved login page to app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+import { LoginComponent } from './shared/components/login/login.component';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
 const routes: Routes = [
   {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
     path: '',
     loadChildren: () => import('./users/users.module').then((m) => m.UsersModule),
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: '**',
-    redirectTo: '',
+    redirectTo: '/login',
   },
 ];
 
